f1113b43aea5f2bd5969e550180ae4930baa664a
Muhammad Tayyab <mtayyab@khi.iba.edu.pk>
2019-07-12 17:46:03 +0500

    mutliple scenarios - 3.2

--- spec.js
+++ spec.js
@@ -1,10 +1,30 @@
 describe('mutliple elements', function(){
-    it('should add one and two', function(){
-        browser.get('http://juliemr.github.io/protractor-demo/');
-        element(by.model('first')).sendKeys(1);
-        element(by.model('second')).sendKeys(2);
-        element(by.id('gobutton')).click();
-        expect(element(by.xpath('/html/body/div/div/form/h2')).getText()).toEqual('3');
 
+    var firstNumber = element(by.model('first'));
+    var secondNumber = element(by.model('second'));
+
+    var gobutton = element(by.id('gobutton'));
+    var resultText = element(by.className('ng-binding'));
+
+
+
+    beforeEach(function(){
+        browser.get('http://juliemr.github.io/protractor-demo/');
+    });
+
+    it('should have a title', function(){
+        expect(browser.getTitle()).toEqual('Super Calculator');
+    });
+
+    it('should add one and two', function(){
+        firstNumber.sendKeys(1);
+        secondNumber.sendKeys(2);
+        gobutton.click();
+        expect(resultText.getText()).toEqual('3');
+    });
+
+    it('should read value from input field', function(){
+        firstNumber.sendKeys(1);
+        expect(resultText.getAttribute()).toEqual('1');
     });
 });
